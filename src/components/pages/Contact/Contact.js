294e45809795f17f4ee01275530bfb899d3c0c33
Van1906 <khachikyan.vanuhi@gmail.com>
2021-01-22 14:54:46 +0400

    v

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -90,8 +90,9 @@ const defaultvalues = {
         e.stopPropagation();
         const isValid = validate();
         if(isValid) {
-            const {name, email, message} = values;
-            props.send({name, email, message});
+            // const {name, email, message} = values;
+            // props.send({name, email, message});
+            props.send(values);
         };
     };
 
